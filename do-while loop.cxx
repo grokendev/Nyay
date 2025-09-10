bec6e3767e5723cb0b4ae7df0a87b00054f16118
grokendev <gonzalesleonard_bsit@plmun.edu.ph>
2025-09-10 21:54:43 +0800

    Add files via upload

---
+++ do-while loop.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    do{
+        
+        cout<< "entered username:" ;
+        cin>> name;
+    }while (name !="Gonzales");
+         cout<< "you entered the exact username: "  << name << endl;
+         
+         
+         return 0;
+    
+}
